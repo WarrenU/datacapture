2ea7d37685b9bdf447e89c3d867a8fc646735a6f
Warren Urbina <warren.urbina@gmail.com>
2020-08-28 07:19:10 -0700

    Update documentation in Node class

--- src/node.py
+++ src/node.py
@@ -2,9 +2,7 @@ class Node():
     """
     Node keeps track of a `value`, and the `occurence` of that value
     within a master data structure. It also keeps track of the amount of
-    values occuring `before` (<) and `after` (>) the `value`. Lastly,
-    it keeps track of the previous node (first value less than it) and
-    the next node (first value that is greater than it)
+    values occuring `before` (<) and `after` (>) the `value`.
     """
 
     def __init__(self, value, occurence=0):
